99f17aa84a1b9bc21c084bfa0665ac3a806c6cba
jodham <wanjalajodham@gmail.com>
2022-07-28 13:16:37 +0300

    updated navbar

--- portal/myapp/views.py
+++ portal/myapp/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-import time
 from django.shortcuts import render, redirect
 from django.views.generic import UpdateView, DetailView, CreateView, ListView
 
--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
